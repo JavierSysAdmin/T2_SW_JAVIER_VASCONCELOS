77d2dc9902145646a607cfe6fa79357ac4bc0f98
JaviersysAdmin <javier_30_01@hotmail.com>
2024-06-23 11:53:41 -0500

    examen 2 inicial

---
+++ src/main/java/cibertec/edu/pe/T2_sw_vasconcelos_javier/T2SwVasconcelosJavierApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_sw_vasconcelos_javier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwVasconcelosJavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SwVasconcelosJavierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/T2_sw_vasconcelos_javier/T2SwVasconcelosJavierApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_sw_vasconcelos_javier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SwVasconcelosJavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
